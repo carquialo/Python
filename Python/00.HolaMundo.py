302ffdddc9b99e017fd57144843f5de727c68067
carquialo <carlitos13219@gmail.com>
2025-05-25 12:56:37 +0200

    Curso de python

---
+++ Python/00.HolaMundo.py
@@ -0,0 +1,23 @@
+#Esto es un comentario: Hola mundo.
+
+print("Hola Mundo")
+
+"""
+Esto es un comentario 
+de varias 
+líneas
+"""
+
+'''
+También decir 
+que se puede 
+poner con comilla simple
+'''
+
+
+
+# Consulta el tipo de dato. 
+print(type("Hola mundo"))   #class 'str'
+print(type(4))              #class 'int' = entero
+print(type(1.5))            #class 'float' = flotante
+print(type(True))           #class 'bool'
---
+++ Python/Exercises.py
@@ -0,0 +1,65 @@
+
+'''
+1. Imprime "¡Hola Mundo!" por consola.
+'''
+
+print("¡Hola Mundo!")
+
+'''
+2. Escribe un comentario de una sola línea explicando qué
+hace el código del Ejercicio 1.
+'''
+
+# El código del ejercicio 1 lo que hace es imprimir por pantalla un texto. 
+
+'''
+3. Imprime tu nombre y edad en la misma línea utilizando la
+función print.
+'''
+
+print("Mi nombre es Carlos y tengo 31 años")
+
+'''
+4. Usa la función type() para imprimir el tipo de dato de
+una cadena de texto, un número entero y un número
+decimal.
+'''
+
+print(type("Hola qué tal?"))
+print(type(6))
+print(type(6.5))
+
+'''
+5. Escribe un comentario en varias líneas explicando qué
+son los tipos de datos en Python.
+'''
+
+'''
+En python los tipos de datos más comunesson: 
+- str = para cadena de texto 
+- int = para números enteros
+- float = números decimales
+- bool = para valores booleanos (True/False)
+'''
+
+'''
+6. Imprime el resultado de concatenar dos cadenas de texto,
+por ejemplo: "Hola" y "Mundo".
+'''
+
+print("Hola" + " " + "Mundo")
+
+'''
+7. Crea una variable para almacenar tu nombre, otra para tu
+edad, e imprime ambas en la misma línea.
+'''
+
+nombre = "Carlos"
+edad = 31
+
+print(nombre + edad)
+
+'''
+8. Escribe un programa que solicite al usuario su nombre y
+lo imprima junto con un saludo.
+'''
