7208e121b5bcafa02ef7224e7a198a9c854e0475
Carlos Quiñones Alonso <carlitos13219@gmail.com>
2026-07-23 11:08:12 +0200

    Eliminar archivos innecesarios

---
+++ Aprendiendo Python/01_datos_primitivos.py
@@ -0,0 +1,27 @@
+###
+# Los tipos de datos primitivos :
+# Son los datos más básicos que el leguaje entiende de forma nativa. Bloques de construcción que no se puede 
+# descomponer.
+
+###
+# Enteros (int) positivo o negativo. 
+print(type(3))
+print(type(-2))
+
+# Con decimales (float).
+print(type(3.4))
+
+# Cadema de texto (str).
+print(type("Hola python"))
+
+# Boleano (bool), si es True o False.
+print(type(True))
+
+# Complejos (complex).
+print(type(1 + 2j))
+
+# Bytes (bytes).
+print(type(b"Hola"))
+
+# Nulo (NoneType)
+print(type(None))
