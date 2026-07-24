30de76e4e12ca04434cfcf878f0d799891d02f23
Carlos Quiñones Alonso <carlitos13219@gmail.com>
2026-07-24 10:08:14 +0200

    datos_compuestos

---
+++ Aprendiendo Python/02_datos_compuestos.py
@@ -0,0 +1,18 @@
+### Datos compuestos ###
+# En python, los datos compuestos son estructuras que pueden almacenar valor.
+
+# Listas (list)
+fruits = ["naranjas","manzanas","peras"]
+print(type(fruits))
+
+# Tuplas (tuple)
+num = (1,2,3,4,5)
+print(type(num))
+
+# Conjunto (sets)
+num = {1,2,3,4,5,6}
+print(type(num))
+
+# Diccionario (Dicctionaries)
+dict = {"Nombre":"Carquialo", "Edad":32}
+print(type(dict))
